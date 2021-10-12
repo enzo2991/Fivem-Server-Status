9017cc65fb85b136be2ce679530ca6933c805a8c
Enzo Galantino <enzo2991@live.fr>
2021-10-12 16:01:03 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -12,65 +12,57 @@ var __importStar = (this && this.__importStar) || function (mod) {
     return result;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+require('dotenv').config();
 const http = __importStar(require("request"));
 const discord = __importStar(require("discord.js"));
 const bot = new discord.Client;
-const config = {
-    "token": "",
-    "guild": "",
-    "staffteam": "",
-    "channels": "",
-    "messsage": '',
-    "refreshtime": 120,
-    "server": "ip:port"
-};
 var interrupteur = 0;
 var firstcmd = 0;
 console.log(`Démarrage du bot...`);
 bot.on("message", message => {
-    if (message.member.roles.has(config.staffteam)) {
-        if (message.content == "!start" && interrupteur == 1) {
+    if(message.member.roles.has(process.env.staffteam)){
+        if(message.content == "!start" && interrupteur == 1){
             if (message.deletable) {
                 message.delete();
             }
-            updatePlayerInterval = setInterval(() => updatePlayers(), config.refreshtime * 1000);
+            updatePlayerInterval = setInterval(() => updatePlayers(), process.env.refreshtime * 1000);
             message.reply("relancement du compteur.").then(msg => {
-                msg.delete(config.refreshtime * 1000)
+                msg.delete(process.env.refreshtime * 1000)
             });
             interrupteur = 0;
-        } else if (message.content == "!start" && interrupteur == 0) {
+        } else if(message.content == "!start" && interrupteur == 0){
             message.delete();
             message.reply("Le bot est deja allumer.").then(msg => {
                 msg.delete(20 * 1000)
             });
         }
-        if (message.content == "!maintenance" && interrupteur == 0) {
+        if(message.content == "!maintenance" && interrupteur == 0){
             if (message.deletable) {
                 message.delete();
             }
-            interrupteur = 1;
+            interrupteur = 1; 
             clearInterval(updatePlayerInterval);
             maintenance();
             message.reply("Mise en maintenance du serveur.").then(msg => {
-                msg.delete(config.refreshtime * 1000)
+                msg.delete(process.env.refreshtime * 1000)
             });
-        } else if (message.content == "!maintenance" && interrupteur == 1) {
+        } else if(message.content == "!maintenance" && interrupteur == 1){
             message.delete();
             message.reply("Le serveur est deja en maintenance.").then(msg => {
                 msg.delete(20 * 1000)
             });
-        }
-        if (message.content == "!first" && firstcmd == 0) {
+        } 
+        if(message.content == "!first" && firstcmd == 0){
             firstcmd = 1;
             message.delete();
-            bot.channels.get(config.channels).send(`Ce message va etre modifier`);
+            bot.channels.get(process.env.channels).send(`Ce message va etre modifier`);
         } else if(message.content == "!first" && firstcmd == 1){
             message.delete();
             message.reply("Le message est deja envoyer.").then(msg => {
                 msg.delete(20 * 1000)
             });
         }
-    } else {
+    } else{
         message.delete();
         message.reply("Tu n'as pas le role necessaire pour utiliser cette commande.").then(msg => {
             msg.delete(20 * 1000)
@@ -78,45 +70,45 @@ bot.on("message", message => {
     }
 });
 
-bot.login(config.token);
+bot.login(process.env.token);
 var updatePlayerInterval;
-updatePlayerInterval = setInterval(() => updatePlayers(), config.refreshtime * 1000);
+updatePlayerInterval = setInterval(() => updatePlayers(), process.env.refreshtime * 1000);
 function updatePlayers() {
-    var args = config.server;
-    let guild = bot.guilds.get(config.guild);
-    http.get(`http://${args}/dynamic.json`, { json: true }, (err, res, data) => {
-        if (err) {
-            if (err.code == "ECONNREFUSED" || err.code == "ETIMEDOUT") {
+        var args = process.env.server;
+        let guild = bot.guilds.get(process.env.guild);
+        http.get(`http://${args}/dynamic.json`, { json: true }, (err, res, data) => {
+            if (err) {
+                if (err.code == "ECONNREFUSED" || err.code == "ETIMEDOUT") {
+                    bot.user.setPresence({
+                        status: "dnd", 
+                        game: {
+                            name: `Serveur Kelloogs Eteint!`, 
+                            type: "Watching", 
+                        }
+                    })
+                  var embed = new discord.RichEmbed()
+                      .setColor('#ff0000')
+                      .addField('**Statut du serveur:**', `❌ Hors Ligne`)
+                      .setTitle('__**Kelloogs  RolePlay**__')
+                      .setImage('https://i.imgur.com/QN6Xg1D.png')
+                      .setTimestamp()
+                      .setFooter('Actualisation','https://i.imgur.com/QN6Xg1D.png');
+                      const maindiscord = bot.guilds.find(g => g.id === process.env.guild); 
+                      const statuschannel = maindiscord.channels.find(c => c.id === process.env.channels); 
+                        statuschannel.fetchMessage(process.env.messsage).then((msg) => { 
+                      msg.edit(embed)
+                  })
+                }
+            }
+            else {
                 bot.user.setPresence({
-                    status: "dnd",
+                    status: "online", 
                     game: {
-                        name: `Serveur Kelloogs Eteint!`,
-                        type: "Watching",
+                        name: `${data.clients} Joueurs sur Kelloogs!`,
+                        type: "Playing", 
                     }
                 })
                 var embed = new discord.RichEmbed()
-                    .setColor('#ff0000')
-                    .addField('**Statut du serveur:**', `❌ Hors Ligne`)
-                    .setTitle('__**Kelloogs  RolePlay**__')
-                    .setImage('https://i.imgur.com/QN6Xg1D.png')
-                    .setTimestamp()
-                    .setFooter('Actualisation', 'https://i.imgur.com/QN6Xg1D.png');
-                const maindiscord = bot.guilds.find(g => g.id === config.guild);
-                const statuschannel = maindiscord.channels.find(c => c.id === config.channels);
-                statuschannel.fetchMessage(config.messsage).then((msg) => {
-                    msg.edit(embed)
-                })
-            }
-        }
-        else {
-            bot.user.setPresence({
-                status: "online",
-                game: {
-                    name: `${data.clients} Joueurs sur Kelloogs!`,
-                    type: "Playing",
-                }
-            })
-            var embed = new discord.RichEmbed()
                 .setColor('#00ff00')
                 .setTitle('__**Kelloogs  RolePlay**__')
                 .addField('**Statut du serveur:**', `✅ En Ligne`, true)
@@ -124,34 +116,34 @@ function updatePlayers() {
                 .addBlankField()
                 .setImage('https://i.imgur.com/QN6Xg1D.png')
                 .setTimestamp()
-                .setFooter('Actualisation', 'https://i.imgur.com/QN6Xg1D.png');
-            const maindiscord = bot.guilds.find(g => g.id === config.guild);
-            const statuschannel = maindiscord.channels.find(c => c.id === config.channels);
-            statuschannel.fetchMessage(config.messsage).then((msg) => {
-                msg.edit(embed)
-            })
-        }
-    });
+                .setFooter('Actualisation','https://i.imgur.com/QN6Xg1D.png');
+                const maindiscord = bot.guilds.find(g => g.id === process.env.guild); 
+                const statuschannel = maindiscord.channels.find(c => c.id === process.env.channels);
+                statuschannel.fetchMessage(process.env.messsage).then((msg) => { 
+                    msg.edit(embed)
+                })
+            }
+        });
 }
 
 function maintenance() {
     bot.user.setPresence({
-        status: "idle",
+        status: "idle", 
         game: {
-            name: `Serveur en maintenance!`,
-            type: "Watching",
+            name: `Serveur en maintenance!`, 
+            type: "Watching", 
         }
-    })
-    var embed = new discord.RichEmbed()
-        .setColor('#FF8000')
-        .addField('**Statut du serveur:**', `🔸 maintenance`)
-        .setTitle('__**KeLLoOgs  RolePlay GTA**__')
-        .setImage('https://i.imgur.com/QN6Xg1D.png')
-        .setTimestamp()
-        .setFooter('Actualisation', 'https://i.imgur.com/QN6Xg1D.png');
-    const maindiscord = bot.guilds.find(g => g.id === config.guild)
-    const statuschannel = maindiscord.channels.find(c => c.id === config.channels);
-    statuschannel.fetchMessage(config.messsage).then((msg) => {
-        msg.edit(embed)
+    })    
+      var embed = new discord.RichEmbed()
+      .setColor('#FF8000')
+      .addField('**Statut du serveur:**', `🔸 maintenance`)
+      .setTitle('__**KeLLoOgs  RolePlay GTA**__')
+      .setImage('https://i.imgur.com/QN6Xg1D.png')
+      .setTimestamp()
+      .setFooter('Actualisation','https://i.imgur.com/QN6Xg1D.png');
+      const maindiscord = bot.guilds.find(g => g.id === process.env.guild) 
+      const statuschannel = maindiscord.channels.find(c => c.id === process.env.channels); 
+      statuschannel.fetchMessage(process.env.messsage).then((msg) => { 
+      msg.edit(embed)
     })
 }
